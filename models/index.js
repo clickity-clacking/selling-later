114779c41e75bddf3bc3c09c6ef28a888b9169b2
sujamanoj <suja@masstv.app>
2022-06-06 19:56:52 -0700

    data for selling later

--- models/index.js
+++ models/index.js
@@ -1,2 +1,14 @@
 // import all models
-const HPost = require('./HPost');
+const { post } = require("../controllers");
+const HPost = require("./HPost");
+const User = require("./user");
+
+post.belongTo(User, {
+  foreignKey: "userId",
+  onDelete: " CASCADE",
+});
+
+module.exports = {
+  User,
+  Post,
+};
