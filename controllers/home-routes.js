855083267838644798d5d393410cd79b0f1636f5
djteiji <djteiji@gmail.com>
2022-06-11 03:04:27 -0700

    listings display to homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,34 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+
+// router.get('/', (req, res) => {
+//   console.log('======================');
+//   res.render('homepage');
+// });
+
+
+const { HPost, User } = require('../models');
 
 router.get('/', (req, res) => {
   console.log('======================');
-  res.render('homepage');
+  HPost.findAll({
+    attributes: ['id', 'address', 'sell_date', 'post_url', 'price_floor', 'price_ceiling', 'beds', 'baths', 'sqft', 'created_at'],
+    order: [['created_at', 'DESC']],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+  .then(dbPostData => {
+    const posts = dbPostData.map(post => post.get({ plain: true }));
+    res.render('homepage', { posts });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
