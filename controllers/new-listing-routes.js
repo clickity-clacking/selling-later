a5a72f72d2670b375b8eb5d157ffa3b0b24165b9
djteiji <djteiji@gmail.com>
2022-06-13 01:38:18 -0700

    display users posts on create listing page

--- controllers/new-listing-routes.js
+++ controllers/new-listing-routes.js
@@ -1,9 +1,49 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User } = require('../models');
+const { HPost, User } = require('../models');
 
+// router.get('/', (req, res) => {
+//     res.render('new-listing');
+//   });
+
+// get all posts for create listings page
 router.get('/', (req, res) => {
-    res.render('new-listing');
-  });
+  console.log(req.session);
+  console.log('======================');
+  HPost.findAll({
+    where: {
+      user_id: req.session.user_id
+    },
+    attributes: [
+      'address',
+      'sell_date',
+      'post_url',
+      'price_floor',
+      'price_ceiling',
+      'beds',
+      'baths',
+      'sqft',
+      'created_at'
+      // 'id',
+      // 'post_url',
+      // 'title',
+      // 'created_at',
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('new-listing', { posts, loggedIn: true });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
