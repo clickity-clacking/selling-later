06f8eb92dbf241bedb1defcb27aa57784633f4ab
clickity-clacking <130marissa.martinez@gmail.com>
2022-06-13 20:21:52 -0700

    switched login/register to templates and added js

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,27 +1,16 @@
 function openLoginForm() {
-  document.getElementById("loginForm").style.display = "block";
-}
-function openRegisterForm() {
-  document.getElementById("registerForm").style.display = "block";
-}
-
-function closeLoginForm() {
-  document.getElementById("loginForm").style.display = "none";
-}
-function closeRegisterForm() {
-  document.getElementById("registerForm").style.display = "none";
+  document.location.assign('/dashboard/');
 }
 
 
-async function loginFormHandler(event) {
-  event.preventDefault();
+async function loginFormHandler() {
 
   const login = document.querySelector('#login').value.trim();
   const password = document.querySelector('#psw').value.trim();
 
   if (login && password) {
     const response = await fetch('/api/users/login', {
-      method: 'post',
+      method: 'get',
       body: JSON.stringify({
         login,
         password
@@ -39,32 +28,6 @@ async function loginFormHandler(event) {
   }
 }
 
-async function signupFormHandler(event) {
-  event.preventDefault();
-
-  const username = document.querySelector('#un').value.trim();
-  const email = document.querySelector('#email').value.trim();
-  const password = document.querySelector('#newPsw').value.trim();
-
-  if (username && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'post',
-      body: JSON.stringify({
-        username,
-        email,
-        password
-      }),
-      headers: { 'Content-Type': 'application/json' }
-    });
-
-    if (response.ok) {
-      document.location.assign('/dashboard/');
-    } else {
-      alert(response.statusText);
-    }
-  }
-}
 
 document.querySelector('.login-form-popup').addEventListener('submit', loginFormHandler);
 
-document.querySelector('.register-form-popup').addEventListener('submit', signupFormHandler);
