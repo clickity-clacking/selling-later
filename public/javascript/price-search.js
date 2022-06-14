9a8d5b1dca37b8a294082b92568746bb1a2fce3f
djteiji <djteiji@gmail.com>
2022-06-14 03:20:35 -0700

    start search

---
+++ public/javascript/price-search.js
@@ -0,0 +1,16 @@
+function priceFormHandler (event) {
+    event.preventDefault();
+
+    console.log("function was called");
+
+    // fetch(`/api/hposts/`) 
+    
+}
+
+
+
+
+
+
+
+document.querySelector('.price-search').addEventListener('change', priceFormHandler);
