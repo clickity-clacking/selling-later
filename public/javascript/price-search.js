b344f1f4fd6ac7f9977eee862fbb0306d2fc6aa4
djteiji <djteiji@gmail.com>
2022-06-15 12:59:51 -0700

    attempt fetch

--- public/javascript/price-search.js
+++ public/javascript/price-search.js
@@ -1,13 +1,41 @@
-function priceFormHandler (event) {
+// function priceFormHandler (event) {
+//     event.preventDefault();
+
+//     console.log("function was called");
+
+//     // fetch(`/api/hposts/`) 
+
+    
+    
+// }
+
+async function priceFormHandler(event) {
     event.preventDefault();
 
-    console.log("function was called");
+    console.log('this button works');
+  
 
-    // fetch(`/api/hposts/`) 
-    
-}
+    const response = await fetch(`/api/hposts`, {
+        attributes: ['id', 'address', 'sell_date', 'post_url', 'price_floor', 'price_ceiling', 'beds', 'baths', 'sqft', 'created_at'],
+        order: [['created_at', 'DESC']],
 
-
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    if (response.ok) {
+      document.querySelector('.all-hposts').style.display = 'none';
+    //   document.location.replace('/listings/');
+       console.log(response.json);
+    // (dbHPostData => {
+    //     const searchposts = dbHPostData.map(searchposts => searchposts.get({ plain: true }));
+    //     console.log(dbHPostData);
+    //     res.render('listings', { searchposts });
+    //   })
+    } else {
+      alert(response.statusText);
+    }
+  }
 
 
 
