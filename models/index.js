e09c52d03120068e3428ef03b1d2dc44d7cb4e93
Erik Dell <edellenator@gmail.com>
2022-06-15 14:39:59 -0700

    geocode and hpost one to one relationship established

--- models/index.js
+++ models/index.js
@@ -3,4 +3,12 @@ const HPost = require('./HPost');
 const User = require('./User');
 const Geocode = require('./Geocode')
 
+HPost.hasOne(Geocode, {
+    foreignKey: 'geocode_id'
+});
+
+Geocode.belongsTo(HPost, {
+    foreignKey: 'geocode_id'
+})
+
 module.exports = { User, HPost, Geocode };
