d4e1eaee1993fd49ba005d9f02df13d25f2bc1da
clickity-clacking <96367186+clickity-clacking@users.noreply.github.com>
2022-06-18 11:42:04 -0700

    Delete selling-later-develop directory

--- selling-later-develop/controllers/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-const userRoutes = require('./user-routes');
-const postRoutes = require('./hpost-routes');
-
-router.use('/users', userRoutes);
-router.use('/hposts', postRoutes);
-
-module.exports = router;
