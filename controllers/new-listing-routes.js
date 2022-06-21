5d9705a0cb41669d3edf1df0a5879847bb9fac32
sujamanoj <suja@masstv.app>
2022-06-21 08:21:23 -0700

    final

--- controllers/new-listing-routes.js
+++ controllers/new-listing-routes.js
@@ -1,30 +1,30 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { HPost, User } = require('../models');
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { HPost, User } = require("../models");
 
 // router.get('/', (req, res) => {
 //     res.render('new-listing');
 //   });
 
 // get all posts for create listings page
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   console.log(req.session);
-  console.log('======================');
+  console.log("======================");
   HPost.findAll({
     where: {
-      user_id: req.session.user_id
+      user_id: req.session.user_id,
     },
     attributes: [
-      'id',
-      'address',
-      'sell_date',
-      'post_url',
-      'price_floor',
-      'price_ceiling',
-      'beds',
-      'baths',
-      'sqft',
-      'created_at'
+      "id",
+      "address",
+      "sell_date",
+      "post_url",
+      "price_floor",
+      "price_ceiling",
+      "beds",
+      "baths",
+      "sqft",
+      "created_at",
       // 'id',
       // 'post_url',
       // 'title',
@@ -33,15 +33,16 @@ router.get('/', (req, res) => {
     include: [
       {
         model: User,
-        attributes: ['username']
-      }
-    ]
+        attributes: ["username"],
+      },
+    ],
   })
-    .then(dbHPostData => {
-      const hposts = dbHPostData.map(hpost => hpost.get({ plain: true }));
-      res.render('new-listing', { hposts, loggedIn: true });
+    .then((dbHPostData) => {
+      const hposts = dbHPostData.map((hpost) => hpost.get({ plain: true }));
+      console.log(hposts);
+      res.render("new-listing", { hposts, loggedIn: true });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
